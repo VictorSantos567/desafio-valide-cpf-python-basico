4f36df8e60b7c75aa1a9a823fa38ff9a6594f265
VictorSantos567 <victoraugustos49@gmail.com>
2022-01-25 22:29:32 -0300

    Refatorando codigo e atualizando readme

--- ValideCPF.py
+++ ValideCPF.py
@@ -1,41 +1,8 @@
-"""
-Para validar o CPF utilizando o digitos
-CPF = 168.995.350-09
-
-- PARA ENCONTRAR O PRIMEIRO DIGITO
-
-multiplicar os 9 primeiro numeros do CPF por contagem regressiva de 10 a 2,
-e a cada multiplicação salvar em uma variavel
-somar todas a variaveis da multiplicação = resultado
-formula
-
-1 * 10 = 10         #   1 * 11 = 11 <-
-6 * 9 = 54          #   6 * 10 = 60
-8 * 8 = 64          #   8 * 9 = 72
-9 * 7 = 63          #   9 * 8 = 72
-9 * 6 = 54          #   9 * 7 = 63
-5 * 5 = 25          #   5 * 6 = 30
-3 * 4 = 12          #   3 * 5 = 15
-5 * 3 = 15          #   5 * 4 = 20
-0 * 2 = 0           #   0 * 3 = 0
-                    #-> 0 * 2 = 0
-        297         #           343
-11 - (297 % 11)= 11 #   11 - (343 % 11) = 9
-11 > 9 = 0          #   Digito 2 = 9
-Digito 1 = 0
-
-11 - (resultado % 11) = 11
-SE 11 > 9 então o resultado é = 0
-digito 1 = 0
-
-- PARA ENCONTRA O SEGUNDO DIGITO
-
-guarda a validação no novo_cpf
-"""
 cpf = '168995350'
 lista = []
 soma = 0
 
+print(f'Os 9 primeiros digitos são: {cpf}')
 
 for cont in cpf:
     cont = int(cont)
@@ -45,17 +12,11 @@ for indice, valor in enumerate(range(10, 1, -1)):
     multiplicacao = lista[indice] * valor
     soma += multiplicacao
 
-# print(f'{soma}')
-
 formula = 11 - (soma % 11)
-# print(digito_1)
-# print(formula)
 
 digito_1 = 0 if formula > 9 else formula
-
-print(digito_1)
-
-print('----------------------------------')
+print(f'Digito 1 encontrado é: {digito_1}')
+"""----------------------------------------------------"""
 
 lista.append(digito_1)
 soma_2 = 0
@@ -64,17 +25,12 @@ for indice, valor in enumerate(range(formula, 1, -1)):
     multiplicacao = lista[indice] * valor
     soma_2 += multiplicacao
 
-# print(soma_2)
-
 formula_2 = 11 - (soma_2 % 11)
-# print(digito_1)
-# print(formula_2)
 
 digito_2 = 0 if formula_2 > 9 else formula_2
-print(digito_2)
 
 lista.append(digito_2)
-print(lista)
+print(f'Digito 2 encontrado é: {digito_2}')
 
 cpf_digito = []
 
@@ -83,12 +39,10 @@ for indice, valor in enumerate(lista):
 
 cpf = "".join(cpf_digito)
 
-# print(type(cpf))
-
-
 novo_cpf = '16899535009'
+print(f'O cpf com digitos é: {novo_cpf}')
 
 if cpf == novo_cpf:
-    print('Válido')
+    print(f'O cpf: {cpf} é Válido')
 else:
-    print("invalido")
+    print(f'O cpf: {cpf} é invalido')
